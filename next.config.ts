3ea7fddfa688973eafa5d591d386ae99f719deee
Vincent550102 <5020559@gmail.com>
2025-07-28 03:03:20 +0800

    Update GitHub page build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const repoName = "injection-monitor"; // GitHub repo name
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
+  basePath: isGitHubPages ? `/${repoName}` : undefined,
+  assetPrefix: isGitHubPages ? `/${repoName}/` : undefined,
 };
 
 export default nextConfig;
